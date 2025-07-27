dea4ae3eab7c5145bc67ba00662cd4798eb805d6
Suyash <tiwarisuyash40@gmail.com>
2025-07-28 03:56:50 +0530

    added gunicorn

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -19,9 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a&@)_-y40mtk0@@6y4%ct$vx&xv&r8*n2i&xe=16_ms#74b^6p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
