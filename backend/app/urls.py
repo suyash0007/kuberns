fbb723db18901bc929c27e5c4591e1e1d677acc2
Suyash Tiwari <tiwarisuyash40@gmail.com>
2025-07-28 22:18:36 +0530

    Update urls.py

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import WebAppCreateView
 
 urlpatterns = [
-    path('webapps/', WebAppCreateView.as_view()),
     path('webapps/', WebAppCreateView.as_view(), name='webapp-list-create')
 ]
